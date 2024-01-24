5ce328bd92f01517bd92bc8de874b3cf3167ab46
InfernalHeir123 <devashishkjoshi1@gmail.com>
2024-01-24 23:33:57 +0000

    Added Motor Connection Code

---
+++ Motor Connection/MotorConnectionMaster/MotorConnectionMaster.ino
@@ -0,0 +1,49 @@
+// If something doesn't work use a 10k pull-up resistor
+// GROUND BOTH ARDUINO's TOGETHER
+// CONNECT BOTH ANALOG PIN A4 AND ANALOG PIN A5
+
+#include <Wire.h>
+
+// Define Slave I2C address and Slave Answer Size
+#define SLAVE_ADDR 9
+#define ANSWER_SIZE 5
+
+void setup() {  
+  Wire.begin();
+
+  Serial.begin(9600);
+  Serial.println("I2C Testing on Arduino");
+}
+
+void loop() {
+    delay(50); //
+    Serial.println("Write Data to Slave");
+
+    Wire.beginTransmission(SLAVE_ADDR);
+    Wire.write(1);
+    Wire.endTransmission();
+
+    // TRY THIS AFTER
+    // delay(50);
+    // Serial.println("Write Data to Slave");
+
+    // uint8_t value = 1;
+    // Wire.beginTransmission(SLAVE_ADDR);
+    // Wire.write((uint8_t*)&value, sizeof(value));
+    // Wire.endTransmission();
+
+    // Read Response from Slave
+    // Read back 5 characters
+    // Don't need right now since we are only using 1 way transmission
+    // Wire.requestFrom(SLAVE_ADDR, ANSWER_SIZE);
+
+    // // Add characters to string
+
+    // String response = "";
+    // while(Wire.available()){
+    //   char b = Wire.read();
+    //   response += b;
+    // }
+
+    // Serial.println(response);
+}
---
+++ Motor Connection/MotorConnectionSlave/MotorConnectionSlave.ino
@@ -0,0 +1,77 @@
+#include <Wire.h>
+
+// Define Slave I2C address and Slave Answer Size
+#define SLAVE_ADDR 9
+#define ANSWER_SIZE 5
+
+int M1dirpin = 7; //Motor X direction pin
+int M1steppin = 6; //Motor X step pin
+int M1en=8; //Motor X enable pin
+int M2dirpin = 4; //Motor Y direction pin
+int M2steppin = 5; //Motor Y step pin
+int M2en=12; //Motor Y enable pin
+
+int Direction = 1; //Control Motor Direction
+int StepRate = 3; //Control Rotational Speed
+
+byte receivedValue = 0; // Variable to store the received value
+
+void setup() {
+  pinMode(M1dirpin,OUTPUT);
+  pinMode(M1steppin,OUTPUT);
+  pinMode(M1en,OUTPUT);
+  pinMode(M2dirpin,OUTPUT);
+  pinMode(M2steppin,OUTPUT);
+  pinMode(M2en,OUTPUT);
+  digitalWrite(M1en,LOW);// Low Level Enable
+  digitalWrite(M2en,LOW);// Low Level 
+
+  Wire.begin(SLAVE_ADDR);
+  // Wire.on Request(requestEvent); // 1 Way connection ONLY
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+  Serial.println("I2C Motor Connection");
+}
+
+void receiveEvent(){
+  // Read while data received
+
+  while (Wire.available() > 0) {
+    receivedValue = Wire.read();
+    Serial.println("Receive Event: " + String(receivedValue));
+  }
+}
+
+
+void loop() {
+  delay(50); // Added the missing semicolon
+
+  if (receivedValue == 1) {
+    int j;
+    int i;
+
+    for (j = 0; j <= 4; j++) {
+      Direction = -Direction;
+      if (Direction < 0) {
+        digitalWrite(M1dirpin, HIGH);
+        digitalWrite(M2dirpin, HIGH);
+      } else {
+        digitalWrite(M1dirpin, LOW);
+        digitalWrite(M2dirpin, LOW);
+      }
+
+      for (i = 0; i <= 1250; i++) {
+        digitalWrite(M1steppin, LOW);
+        digitalWrite(M2steppin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(M1steppin, HIGH); // Rising step
+        digitalWrite(M2steppin, HIGH);
+        delayMicroseconds(StepRate);
+      }
+    }
+
+    // Reset the received value after processing
+    receivedValue = 0;
+  }
+}
+ 
